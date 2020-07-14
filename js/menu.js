3af6e0af2e46a837f81f44e4a14de23511a57280
Arturo <arbarber@ucm.es>
2020-07-14 23:17:26 +0200

    update menu responsive

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,19 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user 
 clicks on the icon */
-function openToggleMenu() {
+function openCloseToggleMenu() {
     var x = document.getElementById("toggleMenu");
     if (x.className === "hamburger hamburger--emphatic") {
       x.className += " is-active";
       document.getElementById("sidebar").style.display = "flex";
+      document.getElementById("contentCover").style.filter = "blur(3px)";
+      document.getElementById("particles-js").style.filter = "blur(3px)";
+      document.body.style.overflowY = "hidden";
+ 
     } else {
       x.className = "hamburger hamburger--emphatic";
       document.getElementById("sidebar").style.display = "none";
-    }
-  }
-
-  function closeToggleMenu() {
-    var x = document.getElementById("toggleMenu");
-    if (x.className === "hamburger hamburger--emphatic") {
-      x.className += " is-active";
-      document.getElementById("sidebar").style.display = "flex";
-    } else {
-      x.className = "hamburger hamburger--emphatic";
-      document.getElementById("sidebar").style.display = "none";
+      document.getElementById("contentCover").style.filter = "none";
+      document.getElementById("particles-js").style.filter = "none";
+      document.body.style.overflowY = "unset";
     }
   }
