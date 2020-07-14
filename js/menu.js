ce85c8ffdb8f571a0dc8ed165be66da568deefec
Arturo <arbarber@ucm.es>
2020-07-15 01:15:22 +0200

    Menú terminado

--- js/menu.js
+++ js/menu.js
@@ -4,14 +4,16 @@ function openCloseToggleMenu() {
     var x = document.getElementById("toggleMenu");
     if (x.className === "hamburger hamburger--emphatic") {
       x.className += " is-active";
-      document.getElementById("sidebar").style.display = "flex";
+      document.getElementById("sidebar").style.visibility ="visible";
+      document.getElementById("sidebar").style.transform = "translateX(0rem)";
       document.getElementById("contentCover").style.filter = "blur(3px)";
       document.getElementById("particles-js").style.filter = "blur(3px)";
       document.body.style.overflowY = "hidden";
  
     } else {
       x.className = "hamburger hamburger--emphatic";
-      document.getElementById("sidebar").style.display = "none";
+      document.getElementById("sidebar").style.visibility ="hidden";
+      document.getElementById("sidebar").style.transform = "translateX(20rem)";
       document.getElementById("contentCover").style.filter = "none";
       document.getElementById("particles-js").style.filter = "none";
       document.body.style.overflowY = "unset";
