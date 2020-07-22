377c07e9102c154f1283a4242c16f5542085d9ae
Arturo <arbarber@ucm.es>
2020-07-23 01:46:29 +0200

    segundo proyecto añadido

--- js/home/hover/hover-projects.js
+++ js/home/hover/hover-projects.js
@@ -2,7 +2,7 @@ var joeAssistant = "#projects .container .content .joeAssistant"
 var joeAssistantCircles = "#projects .container .content .joeAssistant .projectContent .circles"
 var joeAssistantTitle = "#projects .container .content .joeAssistant .projectContent .titleProject"
 var joeAssistantDescription = "#projects .container .content .joeAssistant .projectContent .descripcion"
-var joeAssistantReadMore = "#projects .container .content .projectContent .textoImg .titleTexto .readMore"
+var joeAssistantReadMore = "#projects .container .content .joeAssistant .projectContent .textoImg .titleTexto #readMoreJoeAssistant"
 var joeAssistantImg = "#projects .container .content .joeAssistant .projectContent .textoImg .image > img"
 
 
@@ -10,13 +10,14 @@ var faceDetection = "#projects .container .content .faceDetection"
 var faceDetectionCircles = "#projects .container .content .faceDetection .projectContent .circles"
 var faceDetectionTitle = "#projects .container .content .faceDetection .projectContent .titleProject"
 var faceDetectionDescription = "#projects .container .content .faceDetection .projectContent .descripcion"
-var faceDetectionReadMore = "#projects .container .content .projectContent .textoImg .titleTexto .readMore"
+var faceDetectionReadMore = "#projects .container .content .faceDetection .projectContent .textoImg .titleTexto #readMoreFaceDetection"
 var faceDetectionImg = "#projects .container .content .faceDetection .projectContent .textoImg .image > img"
 
 $(document).ready(function(){
 
     /* JOE ASSISTANT */
     $(joeAssistant).hover(function(){
+        /* CUANDO SE ACTIVA EL HOVER */
             $(joeAssistantCircles.concat(" .circle3")).css("clip-path", "circle(600px at 50% 50%)");
             $(joeAssistantTitle).css("color", "#f9f9f9");
             $(joeAssistantTitle).css("transition", "color 0.45s");
@@ -45,21 +46,30 @@ $(document).ready(function(){
 
         /* FACE DETECTION */
     $(faceDetection).hover(function(){
+        /* CUANDO SE ACTIVA EL HOVER */
             $(faceDetectionCircles.concat(" .circle3")).css("clip-path", "circle(600px at 50% 50%)");
             $(faceDetectionTitle).css("color", "#f9f9f9");
             $(faceDetectionTitle).css("transition", "color 0.45s");
             $(faceDetectionTitle).css("transition", "0.3s");
             $(faceDetectionTitle).css("transform", "translateY(0rem)");
-            $(faceDetectionDescription).css("display", "block");
             $(faceDetectionDescription).css("color", "#f9f9f9");
             $(faceDetectionDescription).css("visibility", "visible");
+            $(faceDetectionReadMore).css("display", "block");
+            $(faceDetectionReadMore).css("color", "#f9f9f9");
+            $(faceDetectionReadMore).css("font-size", "25px");
+            $(faceDetectionReadMore).css("transition", "color 0.45s");
+            $(faceDetectionImg).css("transform", "scale(1.3)");
+            $(faceDetectionImg).css("transition", "transform 0.3s");
     }, function(){
-            $(faceDetectionCircles.concat(" .circle3")).css("clip-path", "circle(150px at 81% 40%)");
+            $(faceDetectionCircles.concat(" .circle3")).css("clip-path", "circle(120px at 81% 40%)");
             $(faceDetectionTitle).css("color", "#ed6f6f");
             $(faceDetectionTitle).css("transition", "0.5s");
-            $(faceDetectionDescription).css("visibility", "hidden");
-            //$(joeAssistantDescription).css("display", "none");
             $(faceDetectionTitle).css("transform", "translateY(3rem)");
+            $(faceDetectionDescription).css("visibility", "hidden");
+            $(faceDetectionReadMore).css("color", "#f9f9f9");
+            $(faceDetectionReadMore).css("display", "none");
+            $(faceDetectionImg).css("transform", "scale(1)");
+            $(faceDetectionImg).css("transition", "transform 0.5s");
         }
     );
 });
