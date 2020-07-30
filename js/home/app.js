e4d80f606086bca78e358512d704b95015c48ae2
Arturo <arbarber@ucm.es>
2020-07-30 15:05:12 +0200

    Portada de los proyectos hecha

--- js/home/app.js
+++ js/home/app.js
@@ -2,7 +2,6 @@
 const JOE_ASSISTANT = "#00c4f8";
 
 
-
 /* Comprueba el tamaño de la pantalla y pone las particulas en consecuencia */
 function checkParticles(path){
   var particles = ""
@@ -26,5 +25,3 @@ function loadingScreen(projectName, color){
     $(".loading-screen .loading-container > h1").css("transition", "opacity 0.3s");
     $(".loading-screen .loading-container > h1").css("opacity", "1");
 }
-
-/* CUANDO SE CAMBIE DE PAGINA O SE VAYA HACIA ATRAS HAY QUE QUITAR LA OPACIDAD DE NUEVO */
